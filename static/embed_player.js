6f944db213d7ed6a3d129411380fcfb124682520
thisissc@gmail.com <thisissc@gmail.com@5c082293-438c-482c-9ae5-221b6ea43d9a>
2013-08-07 01:54:02 +0000

    0.2.1 优化embed_player的事件监听器
    git-svn-id: https://svn.sinaapp.com/thisissc/1@5 5c082293-438c-482c-9ae5-221b6ea43d9a

--- static/embed_player.js
+++ static/embed_player.js
@@ -1,15 +1,87 @@
+console.log = function() {}
+
+var parent_url = 'chrome-extension://gcdebpifkijpiijponofaohmdhahleel/background.html';
+var state_interval_id = null;
+var last_pos = 0;
+var player = null;
+
+function play() {
+    onplaying();
+
+    if (player.controls == undefined) {
+        player.play();
+    } else {
+        player.controls.play();
+    }
+
+    window.clearInterval(state_interval_id);
+    checkstate();
+}
+
+function pause() {
+    onpaused();
+
+    if (player.controls == undefined) {
+        player.stop();
+    } else {
+        player.controls.stop();
+    }
+
+    window.clearInterval(state_interval_id);
+}
+
 function hashchanged() {
     var h = location.hash;
-    var p = document.getElementById('mplayer');
+    console.log(h);
 
     if (h == '#play') {
-        p.controls.play();
+        play();
     } else if (h == '#pause') {
-        p.controls.stop();
+        pause();
     }
 }
 
+function onwaiting() {
+    console.log('==> waiting');
+    parent.location.href = parent_url + '#waiting';
+}
+
+function onplaying() {
+    console.log('==> playing');
+    parent.location.href = parent_url + '#playing';
+}
+
+function onpaused() {
+    console.log('==> pause');
+}
+
+function checkstate() {
+    state_interval_id = window.setInterval(function() {
+        console.log(last_pos);
+        var current_pos = 0;
+        if (player.controls == undefined) {
+            current_pos = player.currentPosition;
+            if (current_pos == undefined) {
+                last_pos = 0;
+                current_pos = 1;
+            }
+        } else {
+            current_pos = player.controls.currentPosition;
+        }
+
+        if (current_pos == last_pos) {
+            onwaiting();
+        } else {
+            onplaying();
+        }
+
+        last_pos = current_pos;
+    }, 600);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+    player = document.getElementById('mplayer');
     window.onhashchange = hashchanged;
+    hashchanged();
 });
 
